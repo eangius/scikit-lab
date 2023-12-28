1b61022535a3d16277d979010a53e1bd8c0d387f
eangius <eliangius@hotmail.com>
2023-12-28 11:08:47 -0500

    reduce default max size to speedup & use less mem

--- tests/common/random_data.py
+++ tests/common/random_data.py
@@ -23,7 +23,7 @@ class RandomData:
         )
 
     @classmethod
-    def geo_points(cls, n_min: int = 0, n_max: int = 10000) -> list:
+    def geo_points(cls, n_min: int = 0, n_max: int = 5000) -> list:
         """ generates a random list of lat/lng points """
         return [
             cls.geo_point()
@@ -31,7 +31,7 @@ class RandomData:
         ]
 
     @classmethod
-    def dense_mtx(cls, row: int = None, col: int = None, n_min: int = 0, n_max: int = 10000):
+    def dense_mtx(cls, row: int = None, col: int = None, n_min: int = 0, n_max: int = 5000):
         """ generates a random (row x col) dense matrix """
         return np.random.rand(
             row or random.randint(n_min, n_max),
@@ -39,7 +39,7 @@ class RandomData:
         )
 
     @classmethod
-    def sparse_mtx(cls, row: int = None, col: int = None, n_min: int = 0, n_max: int = 10000):
+    def sparse_mtx(cls, row: int = None, col: int = None, n_min: int = 0, n_max: int = 5000):
         """ generates a random (row x col) column sparse matrix """
         return scipy.sparse.rand(
             row or random.randint(n_min, n_max),
